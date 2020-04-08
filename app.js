fe9f5eb24392b1564584a1167caa9450e7fabd63
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-04-07 21:37:29 -0700

    entered employee info and function working team members prompts

--- app.js
+++ app.js
@@ -10,9 +10,210 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function startPrompt() {
+    addManager()
+}
+startPrompt()
+
+const employeeInfo = [];
+
+function newTeamMember() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addTeamMember",
+            message: "what type of team member would you like to add",
+            choices: ["Engineer", "Intern", "I don't want to add anymore"]
+        }
+    ])
+        .then(userInput => {
+            if (userInput.addTeamMember === "Engineer") {
+                addEngineer();
+            }
+            else if (userInput.addTeamMember === "Intern") {
+                addIntern();
+            }
+            else if (userInput.addTeamMember === "I don't want to add anymore") {
+                renderHTML();
+                writeToFile(render(employeeInfo));
+            }
+        })
+        .catch(error => {
+            console.log("Was not able to create team-members.html")
+            process.exit(1);
+        });
+}
+
+function renderHTML() {
+    render(employeeInfo);
+}
+
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "manager'",
+            message: "What is your manager's name?"
+
+        },
+        {
+
+            type: "input",
+            name: "managerId",
+            message: "What is your manager's id?"
+
+        },
+        {
+
+            type: "input",
+            name: "managerEmail",
+            message: "What is your manager's email address?"
+
+
+        },
+        {
+
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is your manager's Office number?"
+
+
+        }
+    ])
+        .then(userInput => {
+            let manager = userInput.manager;
+            let managerId = userInput.managerId;
+            let managerEmail = userInput.managerEmail;
+            let managerOfficeNumber = userInput.managerOfficeNumber;
+
+            const managerInfo = new Manager(manager, managerId, managerEmail, managerOfficeNumber)
+            employeeInfo.push(managerInfo)
+            newTeamMember();
+        })
+        .catch(error => {
+            console.log("couldn't add members, please try again")
+            process.exit(1);
+        })
+}
+function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineer'",
+            message: "What is your engineer's name?"
+
+        },
+        {
+
+            type: "input",
+            name: "engineerId",
+            message: "What is your engineer's id?"
+
+        },
+        {
+
+            type: "input",
+            name: "engineerEmail",
+            message: "What is your engineer's email address?"
+
+
+        },
+        {
+
+            type: "input",
+            name: "engineerGithub",
+            message: "What is your engineer's Github username?"
+
+
+        }
+    ])
+        .then(userInput => {
+            let engineer = userInput.engineer;
+            let engineerId = userInput.engineerId;
+            let engineerEmail = userInput.engineerEmail;
+            let engineerGithub = userInput.engineerGithub;
+
+            const engineerInfo = new Engineer(engineer, engineerId, engineerEmail, engineerGithub)
+            employeeInfo.push(engineerInfo)
+            newTeamMember();
+        })
+        .catch(error => {
+            console.log("couldn't add members, please try again")
+            process.exit(1);
+        })
+}
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "intern",
+            message: "What is your intern's name?"
+
+
+        },
+        {
+
+            type: "input",
+            name: "internId",
+            message: "What is your intern's id?"
+
+
+        },
+        {
+
+            type: "input",
+            name: "internEmail",
+            message: "What is your intern's email?"
+
+
+        },
+        {
+
+            type: "input",
+            name: "internSchool",
+            message: "What school does your intern attend?"
+
+
+
+        }
+    ])
+        .then(userInput => {
+            let intern = userInput.intern;
+            let internId = userInput.internId;
+            let internEmail = userInput.internEmail;
+            let internSchool = userInput.internSchool;
+
+            const internInfo = new Intern(intern, internId, internEmail, internSchool)
+            employeeInfo.push(internInfo)
+            newTeamMember();
+        })
+        .catch(error => {
+            console.log("couldn't add members, please try again")
+            process.exit(1);
+        })
+}
+
+
+function writeToFile(data) {
+    return fs.writeFile(outputPath, data, function(error){
+        if (error) {
+            console.log(error)
+            return;
+        }
+        
+        console.log("Information completed!")
+    });
+
+       
+};
+
+
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -32,4 +233,4 @@ const render = require("./lib/htmlRenderer");
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work
